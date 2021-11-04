db0496bfa4d5d43377da467892b0432f95543488
Alex Betros <abet99@Knights.ucf.edu>
2021-11-03 22:19:33 -0400

    November 3, 2021 10:19 PM

--- Assets/Scripts/RubyController.cs
+++ Assets/Scripts/RubyController.cs
@@ -32,4 +32,10 @@ public class RubyController : MonoBehaviour
         position.y = position.y + 3.0f * vertical * Time.deltaTime;
         rigidbody2d.MovePosition(position);
     }
+
+    void ChangeHealth(int amount)
+    {
+        currentHealth = Mathf.Clamp(currentHealth + amount, 0, maxHealth);
+        Debug.Log(currentHealth + "/" + maxHealth);
+    }
 }
