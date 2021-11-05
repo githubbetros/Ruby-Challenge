3b182e761ef308574df215bc9032255873290ffd
Alex Betros <abet99@Knights.ucf.edu>
2021-11-04 21:09:49 -0400

    November 4, 2021 9:09 PM

--- Assets/Scripts/RubyController.cs
+++ Assets/Scripts/RubyController.cs
@@ -8,6 +8,10 @@ public class RubyController : MonoBehaviour
     public int health { get { return currentHealth; } }
     int currentHealth;
 
+    public float timeInvincible = 2.0f;
+    bool isInvincible;
+    float invincibleTimer;
+
     public float speed = 3.0f;
 
     Rigidbody2D rigidbody2d;
