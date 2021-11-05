e063eed37533b6041a798f2b2e75dcb701b2d2ef
Alex Betros <abet99@Knights.ucf.edu>
2021-11-04 21:25:41 -0400

    November 4, 2021 9:25 PM

--- Assets/Scripts/EnemyController.cs
+++ Assets/Scripts/EnemyController.cs
@@ -5,16 +5,20 @@ using UnityEngine;
 public class EnemyController : MonoBehaviour
 {
     public float speed;
-    
+    Rigidbody2D rigidbody2d;
+
     // Start is called before the first frame update
     void Start()
     {
-        
+        rigidbody2d = GetComponent<Rigidbody2D>();
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+        Vector2 position = rigidbody2d.position;
+        position.x = position.x + Time.deltaTime * speed;
+
+        rigidbody2d.MovePosition(position);
     }
 }
