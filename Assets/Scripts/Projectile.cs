8ccbc42ff73d217d453a75e56639ef3ff9c675a8
githubbetros <89561095+githubbetros@users.noreply.github.com>
2021-11-06 17:52:35 -0400

    November 6, 2021 5:52 PM

--- Assets/Scripts/Projectile.cs
+++ Assets/Scripts/Projectile.cs
@@ -4,8 +4,15 @@ using UnityEngine;
 
 public class Projectile : MonoBehaviour
 {
+
+    Rigidbody2D rigidbody2d;
     // Start is called before the first frame update
     void Start()
+    {
+        rigidbody2d = GetComponent<Rigidbody2D>();
+    }
+
+    public void Launch (Vector2 direction, float force)
     {
         
     }
