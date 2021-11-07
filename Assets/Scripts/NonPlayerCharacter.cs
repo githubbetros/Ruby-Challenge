c3bcb2cfc7875f1e00913fc2b9d75556bdc08d84
githubbetros <89561095+githubbetros@users.noreply.github.com>
2021-11-07 14:28:36 -0500

    November 7, 2021 2:28 PM

--- Assets/Scripts/NonPlayerCharacter.cs
+++ Assets/Scripts/NonPlayerCharacter.cs
@@ -4,10 +4,14 @@ using UnityEngine;
 
 public class NonPlayerCharacter : MonoBehaviour
 {
+    public float displayTime = 4.0f;
+    public GameObject dialogBox;
+    float timerDisplay;
     // Start is called before the first frame update
     void Start()
     {
-        
+        dialogBox.SetActive(false);
+        timerDisplay = -1.0f;
     }
 
     // Update is called once per frame
