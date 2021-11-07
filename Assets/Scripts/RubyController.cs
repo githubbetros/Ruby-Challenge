1b21242b81e630e22de2e293c14b4b846e5d47cc
githubbetros <89561095+githubbetros@users.noreply.github.com>
2021-11-07 14:31:22 -0500

    November 7, 2021 2:31 PM

--- Assets/Scripts/RubyController.cs
+++ Assets/Scripts/RubyController.cs
@@ -63,12 +63,12 @@ public class RubyController : MonoBehaviour
             Launch();
         }
 
-        if (hit.collider != null)
+        if (Input.GetKeyDown(KeyCode.X))
         {
-            NonPlayerCharacter character = hit.collider.GetComponent<NonPlayerCharacter>();
-            if (character != null)
+            RaycastHit2D hit = Physics2D.Raycast(rigidbody2d.position + Vector2.up * 0.2f, lookDirection, 1.5f, LayerMask.GetMask("NPC"));
+            if (hit.collider != null)
             {
-                character.DisplayDialog();
+                Debug.Log("Raycast has hit the object " + hit.collider.gameObject);
             }
         }
     }
